aa11d6109c1d48e901bbb5a8fb1eabffd20a6c84
Yannick Lml <yle-mouel@laposte.net>
2022-12-27 19:39:34 +0100

    after netlify add

--- pages/Login.vue
+++ pages/Login.vue
@@ -2,12 +2,17 @@
     <div>
         <h1 class="text-center">Login View</h1>
         <p v-if="user">{{ user.email }}</p>
-        <p v-else-if="state.errorCode">{{ state.errorCode }}</p>
+        <p v-if="state.errorCode">{{ state.errorCode }}</p>
 
         <div class="flex max-w-2xl mx-auto">
             <button @click="signIn"  class="bg-green-400 py-2 px-4  rounded-md mx-auto block">Sign In</button>
             <button @click="signOut" class="bg-blue-400 py-2 px-4  rounded-md mx-auto block">Sign Out</button>
         </div>
+
+        <pre v-if="user">
+            {{ user }}
+        </pre>
+
     </div>
 </template>
 
@@ -18,6 +23,7 @@
         password:''
     })
 
+    // equivalent de son credentials = ref();
     const user = ref();
 
     const state = reactive({
